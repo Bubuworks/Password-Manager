b2c5460b94c650bc09f6f763c4baef1e3a23a090
Bubuworks <bubupersonal@tutamail.com>
2026-02-07 16:21:23 +0000

    Create Argon2Kdf.cs

---
+++ src/PasswordManager/Crypto/Argon2Kdf.cs
@@ -0,0 +1,30 @@
+using Konscious.Security.Cryptography;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordManager.Crypto;
+
+public static class Argon2Kdf
+{
+    public static byte[] DeriveKey(ReadOnlySpan<char> password, byte[] salt)
+    {
+        var pwdBytes = Encoding.UTF8.GetBytes(password.ToArray());
+
+        try
+        {
+            using var argon2 = new Argon2id(pwdBytes)
+            {
+                Salt = salt,
+                Iterations = 4,
+                MemorySize = 256 * 1024,
+                DegreeOfParallelism = Environment.ProcessorCount
+            };
+
+            return argon2.GetBytes(32);
+        }
+        finally
+        {
+            CryptographicOperations.ZeroMemory(pwdBytes);
+        }
+    }
+}
