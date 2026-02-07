07b2a70b58ccf4f787e362ad92951758b7bfde1c
Bubuworks <bubupersonal@tutamail.com>
2026-02-07 16:01:50 +0000

    Create Program.cs

---
+++ src/PasswordManager/Program.cs
@@ -0,0 +1,30 @@
+using SecurePasswordManager.Vault;
+
+Console.Write("Master password: ");
+var password = ReadPassword();
+
+var vault = Vault.CreateNew(password);
+vault.AddEntry("github.com", "user@example.com", "supersecret");
+
+vault.Save("vault.bin");
+
+Console.WriteLine("Vault saved.");
+
+static char[] ReadPassword()
+{
+    var pwd = new Stack<char>();
+    ConsoleKeyInfo key;
+
+    while ((key = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+    {
+        if (key.Key == ConsoleKey.Backspace && pwd.Count > 0)
+        {
+            pwd.Pop();
+            continue;
+        }
+        pwd.Push(key.KeyChar);
+    }
+
+    Console.WriteLine();
+    return pwd.Reverse().ToArray();
+}
