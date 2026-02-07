d033e43bd5458c360b097ba660650e8ee540f0a1
Bubuworks <bubupersonal@tutamail.com>
2026-02-07 17:17:31 +0000

    Create RandomUtil.cs

---
+++ src/PasswordManager/Utils/RandomUtil.cs
@@ -0,0 +1,20 @@
+using System.Security.Cryptography;
+
+namespace PasswordManager.Utils;
+
+public static class RandomUtil
+{
+    public static string GeneratePassword(int length = 20)
+    {
+        const string chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+
+        var bytes = RandomNumberGenerator.GetBytes(length);
+        var result = new char[length];
+
+        for (int i = 0; i < length; i++)
+            result[i] = chars[bytes[i] % chars.Length];
+
+        return new string(result);
+    }
+}
