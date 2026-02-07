79d4aee3f2ee450776306348df653cce65d697b6
Bubuworks <bubupersonal@tutamail.com>
2026-02-07 23:24:18 +0000

    Update Program.cs

--- src/PasswordManager/Program.cs
+++ src/PasswordManager/Program.cs
@@ -13,30 +13,50 @@
 //              `-'  `--'           `-'  `--'                                 '------'  '---'          
 
 using PasswordManager.Vault;
+using TextCopy;
+using System.Timers;
 
 const string VaultPath = "vault.bin";
+const int AutoLockTimeoutMs = 60000;
 
+Vault vault;
 Console.Write("Master password: ");
 var masterPassword = ReadPassword();
 
-Vault vault = File.Exists(VaultPath)
-    ? Vault.Load(masterPassword, VaultPath)
-    : Vault.CreateNew(masterPassword);
+vault = File.Exists(VaultPath) ? Vault.Load(masterPassword, VaultPath) : Vault.CreateNew(masterPassword);
+
+var inactivityTimer = new Timer(AutoLockTimeoutMs);
+inactivityTimer.Elapsed += (_, _) =>
+{
+    Console.WriteLine("\nVault auto-locked due to inactivity.\n");
+    vault = null!;
+    ClipboardService.SetText(string.Empty);
+};
+inactivityTimer.Start();
 
 bool running = true;
 while (running)
 {
-    Console.WriteLine("""
-    1) Add entry
-    2) List entries
-    3) View entry
-    4) Update entry
-    5) Delete entry
-    6) Save & exit
-    """);
+    if (vault == null)
+    {
+        Console.Write("Vault locked. Re-enter master password: ");
+        masterPassword = ReadPassword();
+        try
+        {
+            vault = Vault.Load(masterPassword, VaultPath);
+        }
+        catch
+        {
+            Console.WriteLine("Incorrect password.");
+            continue;
+        }
+    }
 
+    ShowMenu();
     Console.Write("Select option: ");
     var choice = Console.ReadLine();
+    inactivityTimer.Stop();
+    inactivityTimer.Start();
 
     switch (choice)
     {
@@ -56,23 +76,36 @@ while (running)
             DeleteEntry(vault);
             break;
         case "6":
+            CopyPassword(vault);
+            break;
+        case "7":
             vault.Save(VaultPath);
             running = false;
             break;
     }
 }
 
+static void ShowMenu()
+{
+    Console.WriteLine("""
+    1) Add entry
+    2) List entries
+    3) View entry
+    4) Update entry
+    5) Delete entry
+    6) Copy password to clipboard
+    7) Save & exit
+    """);
+}
+
 static void AddEntry(Vault vault)
 {
     Console.Write("Site: ");
     var site = Console.ReadLine() ?? "";
-
     Console.Write("Username: ");
     var username = Console.ReadLine() ?? "";
-
     Console.Write("Password: ");
     var password = ReadPassword();
-
     vault.AddEntry(site, username, new string(password));
 }
 
@@ -80,13 +113,10 @@ static void UpdateEntry(Vault vault)
 {
     Console.Write("Site to update: ");
     var site = Console.ReadLine() ?? "";
-
     Console.Write("New username: ");
     var username = Console.ReadLine() ?? "";
-
     Console.Write("New password: ");
     var password = ReadPassword();
-
     vault.UpdateEntry(site, username, new string(password));
 }
 
@@ -107,17 +137,29 @@ static void ViewEntry(Vault vault)
 {
     Console.Write("Site: ");
     var site = Console.ReadLine() ?? "";
-
     var entry = vault.FindEntry(site);
     if (entry != null)
         Console.WriteLine($"{entry.Username} / {entry.Password}");
+}
+
+static void CopyPassword(Vault vault)
+{
+    Console.Write("Site: ");
+    var site = Console.ReadLine() ?? "";
+    var entry = vault.FindEntry(site);
+    if (entry == null) return;
+    ClipboardService.SetText(entry.Password);
+    _ = Task.Run(async () =>
+    {
+        await Task.Delay(10000);
+        ClipboardService.SetText(string.Empty);
+    });
 }
 
 static char[] ReadPassword()
 {
     var buffer = new List<char>();
     ConsoleKeyInfo key;
-
     while ((key = Console.ReadKey(true)).Key != ConsoleKey.Enter)
     {
         if (key.Key == ConsoleKey.Backspace && buffer.Count > 0)
@@ -125,7 +167,6 @@ static char[] ReadPassword()
         else if (!char.IsControl(key.KeyChar))
             buffer.Add(key.KeyChar);
     }
-
     Console.WriteLine();
     return buffer.ToArray();
 }
